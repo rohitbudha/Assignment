159f5c1f608e736317cde4c0b71c376ce2ff7a31
rohitbudha <robitsaun9988@gmail.com>
2024-08-27 17:21:02 +0545

    Delete com/example/domain directory

--- com/example/domain/Engineer.java
+++
@@ -1,9 +0,0 @@
-package com.example.domain;
-
-public class Engineer extends Employee {
-	
-	public Engineer(int empid, String name, String ssn, double salary) {
-		super(empid, name, ssn, salary);
-		
-	}
-}
